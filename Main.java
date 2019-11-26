0b4febf5a1bcd80f0996a6b49802e9db655e3eb3
Sarat Chandra Sekhar Ryali <ryaliscs@gmail.com>
2019-11-26 08:09:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import in.sar.Hello;
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    Hello h = new Hello();    
+    System.out.println(h.sayHello("Sarat"));
+  }
+}
---
+++ jdt.ls-java-project/src/in/sar/Hello.java
@@ -0,0 +1,8 @@
+package in.sar;
+
+public class Hello {
+    public String sayHello(String aName)
+    {
+      return "Hello "+aName;
+    }
+}
